f55f8f5abcd6f0bd1ae41589ed282fb4fd6e2235
Frank Hunleth <fhunleth@troodon-software.com>
2016-07-05 17:02:46 -0400

    Add more options to the Elixir twiddler

--- lib/gpio_twiddler/elixir.ex
+++ lib/gpio_twiddler/elixir.ex
@@ -1,24 +1,44 @@
 defmodule GpioTwiddler.Elixir do
 
- def twiddle(count) do
+  @doc """
+  Twiddle the GPIO. Example calls:
+
+     twiddle(20000, [], :write)  # Open in normal mode, call IO.write/2
+     twiddle(20000, [], :binwrite)  # Open in normal mode, call IO.binwrite/2
+     twiddle(20000, [:raw], :binwrite)  # Open in raw mode, call IO.binwrite/2
+     twiddle(20000, [:raw], :binwrite_list)  # Open in raw mode, call IO.binwrite/2, pass list
+  """
+  def twiddle(count, options \\ [], twiddler_type \\ :binwrite) do
     export_pin()
 
-    File.open("/sys/class/gpio/gpio60/value", [:write],
-              fn(file) -> twiddle_again(file, count) end)
+    twiddler = get_twiddler(twiddler_type)
+    File.open("/sys/class/gpio/gpio60/value", [:write] ++ options,
+              &(twiddler.(&1, count)))
   end
 
-  def twiddle_raw(count) do
-     export_pin()
+  defp get_twiddler(:write), do: &twiddle_write_again/2
+  defp get_twiddler(:binwrite), do: &twiddle_binwrite_again/2
+  defp get_twiddler(:binwrite_list), do: &twiddle_binwrite_list_again/2
 
-     File.open("/sys/class/gpio/gpio60/value", [:write, :raw],
-               fn(file) -> twiddle_again(file, count) end)
-   end
+  defp twiddle_binwrite_again(_file, 0), do: :ok
+  defp twiddle_binwrite_again(file, count) do
+    IO.binwrite(file, "1\n")
+    IO.binwrite(file, "0\n")
+    twiddle_binwrite_again(file, count - 1)
+  end
 
-  defp twiddle_again(_file, 0), do: :ok
-  defp twiddle_again(file, count) do
+  defp twiddle_binwrite_list_again(_file, 0), do: :ok
+  defp twiddle_binwrite_list_again(file, count) do
+    IO.binwrite(file, '1\n')
+    IO.binwrite(file, '0\n')
+    twiddle_binwrite_again(file, count - 1)
+  end
+
+  defp twiddle_write_again(_file, 0), do: :ok
+  defp twiddle_write_again(file, count) do
     IO.write(file, "1\n")
     IO.write(file, "0\n")
-    twiddle_again(file, count - 1)
+    twiddle_write_again(file, count - 1)
   end
 
   def export_pin() do
