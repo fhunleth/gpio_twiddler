bd66d13a37aec24a65c919ac171b0b786b1e3562
Frank Hunleth <fhunleth@troodon-software.com>
2016-07-06 17:37:33 -0400

    Add functions to set the priority

--- lib/gpio_twiddler.ex
+++ lib/gpio_twiddler.ex
@@ -1,20 +1,13 @@
 defmodule GpioTwiddler do
-  use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
-  def start(_type, _args) do
-    import Supervisor.Spec
-
-    # Define workers and child supervisors to be supervised
-    children = [
-      # worker(GpioTwiddler.Worker, [arg1, arg2, arg3]),
-    ]
-
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: GpioTwiddler.Supervisor]
-    Supervisor.start_link(children, opts)
+  @doc """
+  Currently, this just sets the current thread to high priority.
+  """
+  def set_high_priority() do
+    Process.flag(:priority, :high)
+  end
+  def set_normal_priority() do
+    Process.flag(:priority, :normal)
   end
 
 end
